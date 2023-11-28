eda4b4e49037b1cd575c2f63b3493fa628d7cbc8
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-11-28 19:48:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const parseCode = (str) => {
   // your code here
+	const arr = str.split('/0+/');
+	const obj = {
+		firstName: arr[0],
+		lastName: arr[1],
+		id: arr[2],
+	}
+	return obj;
 };
 
 // Do not change the code below
-const str = prompt("Enter str: ");
-alert(JSON.stringify(parseCode(str)));
+// const str = prompt("Enter str: ");
+// alert(JSON.stringify(parseCode(str)));
